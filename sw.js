a759b4bcea439b8ef6eda9629e2d117b1ef2be21
NH4mid2019 <admin@nurident.com>
2021-07-31 22:01:56 +0300

    sss

--- sw.js
+++ sw.js
@@ -1,24 +1,16 @@
-const cacheName = 'nikkaranco'
-
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(['/mobile', '/about-mobile',]);
-  })());
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      '/mobile',
+      '/about-mobile',
+
+    ])),
+  );
 });
 
-// Fetching content using Service Worker
 self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) return r;
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
 });
